6e1dc86440a1188a20f38a8f62ad143eead942e0
Alhaimouni <m.alhaumouni@gmail.com>
2022-09-21 23:19:42 +0300

    add

--- models/index.js
+++ models/index.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
-const { Sequelize, DataTypes } = require('sequelize');
-const { createPostsTable } = require('./post.model');
-const { createCommentTable } = require('./comment.model');
-const { createUsersTable } = require('./users.model');
-const { CrudOperations } = require('../collections/user-comment-routes');
+const { Sequelize, DataTypes } = require("sequelize");
+const { createPostsTable } = require("./post.model");
+const { createCommentTable } = require("./comment.model");
+const { createUsersTable } = require("./users.model");
+const { CrudOperations } = require("../collections/user-comment-routes");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const POSTGRES_URL = process.env.DATABASE_URL;
 
 let sequelizeOptions = {
-
-    dialectOptions : {
-        ssl : {
-            require : false,
-            rejectUnauthorized: false
-        }
-    }
-
+  dialectOptions: {
+    ssl: {
+      require: false,
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Database connected to postgres DBMS');
-    }).catch((err) => {
-        console.log(err);
-    });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected to postgres DBMS");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const postsModel = createPostsTable(sequelize, DataTypes);
 const posts = new CrudOperations(postsModel);
@@ -38,20 +38,18 @@ const comments = new CrudOperations(commentModel);
 
 const usersModel = createUsersTable(sequelize, DataTypes);
 
+postsModel.hasMany(commentModel, { foreignKey: "textId", sourceKey: "id" });
+commentModel.belongsTo(postsModel, { foreignKey: "textId", targetKey: "id" });
 
-postsModel.hasMany(commentModel, { foreignKey: 'textId', sourceKey: 'id' });
-commentModel.belongsTo(postsModel, { foreignKey: 'textId', targetKey: 'id' });
-
-
+usersModel.hasMany(commentModel, { foreignKey: "userId", sourceKey: "id" });
+commentModel.belongsTo(usersModel, { foreignKey: "userId", targetKey: "id" });
 
 
 module.exports = {
-
-    posts,
-    comments,
-    postsModel,
-    usersModel,
-    commentModel,
-    dataBase: sequelize,
-
-}
+  posts,
+  comments,
+  postsModel,
+  usersModel,
+  commentModel,
+  dataBase: sequelize,
+};
